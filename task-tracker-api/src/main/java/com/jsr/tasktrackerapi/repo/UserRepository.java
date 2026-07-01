bb9c9a2a5b177cae0944eea028e73bca257137c5
Jasur <jasurnabijoniv@gmail.com>
2026-07-02 01:33:05 +0500

    feat: добавил jpa интерфейсы для обеих сущностей(user, task) в пакете repo

---
+++ task-tracker-api/src/main/java/com/jsr/tasktrackerapi/repo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jsr.tasktrackerapi.repo;
+
+import com.jsr.tasktrackerapi.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
